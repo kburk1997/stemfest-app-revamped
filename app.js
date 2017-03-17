d921fe41b705b8cb078355d986cfa8f5a5884318
kburk1997 <kburk97@optonline.net>
2017-03-17 18:27:27 -0400

    working sidebar

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var app = angular.module('stemfestApp', [
   'ngRoute',
-  'mobile-angular-ui.gestures',
+  'mobile-angular-ui',
   'mobile-angular-ui.gestures'
 ]);
 
 app.run(function($transform) {
   window.$transform = $transform;
+});
+
+app.config(function($routeProvider) {
+  $routeProvider.when('/', {templateUrl: 'assets/templates/home.html', reloadOnSearch: false});
+  $routeProvider.when('/about', {templateUrl: 'assets/templates/about.html', reloadOnSearch: false});
+  $routeProvider.when('/directions-parking', {templateUrl: 'assets/templates/directions_parking.html', reloadOnSearch: false});
+  $routeProvider.when('/map', {templateUrl: 'assets/templates/about.html', reloadOnSearch: false});
+  $routeProvider.when('/schedule', {templateUrl: 'assets/templates/schedule.html', reloadOnSearch: false});
+
 });
